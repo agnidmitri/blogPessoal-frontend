35b90b7a52e46ee6f6caedcdd1cae158bb82e7f3
agnidmitri <limadmitri@protonmail.com>
2021-06-08 21:32:36 -0300

    get/post postagem

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { environment } from 'src/environments/environment.prod';
+import { Post } from '../model/Post';
+import { Tema } from '../model/Tema';
+import { User } from '../model/User';
+import { PostService } from '../service/post.service';
+import { TemaService } from '../service/tema.service';
 
 @Component({
   selector: 'app-home',
@@ -9,16 +14,57 @@ import { environment } from 'src/environments/environment.prod';
 })
 export class HomeComponent implements OnInit {
 
+  post: Post = new Post()
+
+  tema: Tema = new Tema()
+  listaTemas: Tema[]
+  idTema: number
+
+  user: User = new User()
+  idUser = environment.id
+
   constructor(
-    private router: Router
-  ){ }
+    private router: Router,
+    private postService: PostService,
+    private temaService: TemaService
+  ) { }
 
   ngOnInit() {
+
+    if (environment.token == '') {
+
+      this.router.navigate(['/login'])
+    }
+
+    this.getAllTemas()
+  }
+
+  getAllTemas(){
+      this.temaService.getAllTema().subscribe((resp: Tema[])=> {
+        this.listaTemas = resp
+      })
+    }
+
+  findByIdTema(){
+    this.temaService.getByIdTema(this.idTema).subscribe((resp: Tema) =>{
+      this.tema = resp
+    })
+  }
+
+  publicar(){
+    this.tema.id = this.idTema
+    this.post.tema = this.tema
   
-  if (environment.token ==''){
-    
-    this.router.navigate(['/login'])
+    this.user.id = this.idUser
+    this.post.usuario = this.user
+
+    this.postService.postPost(this.post).subscribe((resp: Post) =>{
+      this.post = resp
+      alert('Sua nova publicação foi criada!')
+    })
+
+
   }
-  }
+
 
 }
